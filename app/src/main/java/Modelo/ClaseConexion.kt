ca5322839ec9c9b0ce5e14f8af768929bfff3ff9
Steven <20220721@ricaldone.edu.sv>
2024-06-20 13:48:48 -0600

    ip

--- app/src/main/java/Modelo/ClaseConexion.kt
+++ app/src/main/java/Modelo/ClaseConexion.kt
@@ -6,7 +6,7 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion(): Connection? {
         try {
-            val url = "jdbc:oracle:thin:@192.168.1.2:1521:xe"
+            val url = "jdbc:oracle:thin:@192.168.1.5:1521:xe"
             val usuario = "system"
             val contrasena = "desarrollo"
 
